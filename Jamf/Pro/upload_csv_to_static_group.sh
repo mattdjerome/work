728093d277f35fa14c6f3c02dd0c930cd0cd9d53
mattdjerome <mattdjerome@gmail.com>
2025-02-20 12:24:17 -0800

    read the 1st column of a csv and add to a static group

---
+++ Jamf/Pro/upload_csv_to_static_group.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#API login info
+apiuser="$1"
+apipass="$2"
+jamfProURL="$3"
+sourceFile="$4"
+
+# Obtain a Bearer Token using Basic Authentication, write the output into a variable
+request=$(
+        /usr/bin/curl --request POST \
+                                    --url ${jamfProURL}/api/v1/auth/token \
+                                    --header 'accept: application/json' \
+                                    --user "${apiuser}:${apipass}"
+)
+
+# Extract the token from the JSON
+token=$(/usr/bin/plutil -extract token raw -o - - <<< "$request")
+
+GroupID="$5"
+apiURL="${jamfProURL}/JSSResource/computergroups/id/${GroupID}"
+
+while IFS=',' read -r col1
+do
+    xmlHeader='<?xml version="1.0" encoding="UTF-8" standalone="no"?>'
+    apiData="<computer_group><id>$GroupID</id><name>Umbrella test</name><computer_additions><computer><name>$col1</name></computer></computer_additions></computer_group>"
+    curl -X PUT "${apiURL}" -H "Authorization: Bearer ${token}" -H "Content-Type: application/xml" -d "${xmlHeader}${apiData}" 
+done < $sourceFile
