c683cb9956a59819bb7c82115ca8d09c6f81a604
mattdjerome <mattdjerome@gmail.com>
2025-05-12 09:01:09 -0700

    added comments on best practice

--- Jamf/Pro/upload_csv_to_static_group.sh
+++ Jamf/Pro/upload_csv_to_static_group.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# IMPORTANT NOTE: For this to work, the computer names must be in a CSV with no other data. Otherwise it'll error out. Also, don't rename an xls to csv. Create a whole new document.
 
 #API login info
 apiuser="$1"
@@ -15,9 +16,9 @@ token=$(/usr/bin/plutil -extract token raw -o - - <<< "$request")
 GroupID="$5"
 GroupName="$6"
 apiURL="${jamfProURL}/JSSResource/computergroups/id/${GroupID}"
-open $apiURL
 while IFS=',' read -r col1
 do
+    echo $col1
     xmlHeader='<?xml version="1.0" encoding="UTF-8" standalone="no"?>'
     apiData="<computer_group><id>$GroupID</id><name>$GroupName</name><computer_additions><computer><name>$col1</name></computer></computer_additions></computer_group>"
     curl -X PUT "${apiURL}" -H "Authorization: Bearer ${token}" -H "Content-Type: application/xml" -d "${xmlHeader}${apiData}"
